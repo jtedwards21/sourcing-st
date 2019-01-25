f54300cb4ca8d8b88cb0f8db8a16e86889bb5c52
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-25 20:36:46 +0800

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ import logo from '../assets/images/logo.svg';
 
 const Footer = (props) => (
     <footer id="footer">
-        <section>
+        <section className="box white-box">
             <h2>Who we are</h2>
             <p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>
         </section>
-        <section>
+        <section className="box white-box">
             <h2>Contact Information</h2>
             <dl className="alt">
                 <dt>Address</dt>
@@ -19,13 +19,6 @@ const Footer = (props) => (
                 <dt>Email</dt>
                 <dd><a href="#">information@untitled.tld</a></dd>
             </dl>
-            <ul className="icons">
-                <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook alt"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon fa-dribbble alt"><span className="label">Dribbble</span></a></li>
-            </ul>
         </section>
         <p className="copyright">&copy; Stellar Sourcing</p>
     </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import logo from '../assets/images/logo.svg';
+import brp from '../assets/images/blue_river_side_text.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
         <div className="box white-box">
-        <h1>DS - Delta Sourcing</h1>
+        <span className="image"><img src={brp} alt="" /></span>
         <p>Sourcing agents to help you do more<br />
         with your business.</p>
         </div>
