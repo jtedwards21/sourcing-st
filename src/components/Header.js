40b54a689e2308379f231cc77e44932822cfb49f
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-25 18:01:47 +0800

    added a white-box elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import logo from '../assets/images/logo.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
+        <div className="box white-box">
         <h1>DS - Delta Sourcing</h1>
         <p>Sourcing agents to help you do more<br />
         with your business.</p>
+        </div>
     </header>
 )
 
