b8d38e29b376dc60f0693f757c57d7443586de8d
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-25 11:29:52 +0800

    title edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import logo from '../assets/images/logo.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <h1>Export Checkout</h1>
-        <p>Export agents to help you do more<br />
+        <h1>DS - Delta Sourcing</h1>
+        <p>Sourcing agents to help you do more<br />
         with your business.</p>
     </header>
 )
