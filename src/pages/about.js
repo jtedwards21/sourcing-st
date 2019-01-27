583e190fb5c8e522472896adb14ee3c8e9308ac9
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-27 09:20:23 +0800

    edited about page and services page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import AboutHeader from '../components/AboutHeader'
 import pic04 from '../assets/images/pic04.jpg'
+import mappic from '../assets/images/mappic01.png'
 
 class Generic extends React.Component {
   render() {
@@ -14,12 +15,12 @@ class Generic extends React.Component {
         <AboutHeader />
         <div id="main">
           <section id="content" className="main">
-            <span className="image main"><img src={pic04} alt="" /></span>
-            <h2>Magna feugiat lorem</h2>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+            <span className="image main"><img src={mappic} alt="" /></span>
+            <h2>Company Location</h2>
+            <p>We are located in Jiaxing, just outside of Shanghai. This allows for convenient travel and shipping from the surrounding area, including Suzhou, Hangzhou, Wuxi, and Ningbo. Goods from this area account for about a third of China's exports.</p>
+            <p>Many of our services concentrate on products and companies in this area, and includes products from the automotive, textile, and electronics industry, among others.</p>
+            <h2>Industry Experience</h2>
+            <p>We have been </p>
           </section>
         </div>
       </Layout>
--- src/pages/services.js
+++ src/pages/services.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import ServicesHeader from '../components/ServicesHeader'
-import pic04 from '../assets/images/pic04.jpg'
+import pic57 from '../assets/images/pic57.jpg'
 
 class Generic extends React.Component {
   render() {
@@ -14,12 +14,15 @@ class Generic extends React.Component {
         <ServicesHeader />
         <div id="main">
           <section id="content" className="main">
-            <span className="image main"><img src={pic04} alt="" /></span>
-            <h2>Magna feugiat lorem</h2>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+            <span className="image main"><img src={pic57} alt="" /></span>
+            <h2>Product Sourcing</h2>
+            <p>Many companies and individuals don't know how to find suppliers, or don't know how to choose among the many are available. We will help you find an appropriate manufacturer and get their quotation in less than 2 working days.</p>
+            <p>If you'd previously had a manufacturer that you weren't satisfied with, we can help you find someone with better price and quality.</p>
+            <h2>Product Samples</h2>
+            <p>If you're located outside of China, getting samples can often be a waste of time and money.</p>
+            <p>It's more time- and cost-efficient for us to receive your samples for you. Anything that doesn't meet basic standards with not be shipped to you and the rest will be consolidated.</p>
+            <h2>Product Samples</h2>
+            <p>If you're located outside of China, getting samples can often be a waste of time and money.</p>
           </section>
         </div>
       </Layout>
